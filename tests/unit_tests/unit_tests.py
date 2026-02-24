dea0b8fd135a70434c62765d809c99d11b6221c1
Lounis Hamroun@gmail.com <lounis.hamroun@gmail.com>
2026-02-24 16:15:56 +0100

    init

---
+++ tests/unit_tests/unit_tests.py
@@ -0,0 +1,14 @@
+import unittest
+import requests
+import src.helpers
+import sys 
+
+
+
+class TestExample(unittest.TestCase):
+    def request_to_weather_api():
+        pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
